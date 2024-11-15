ef0d3a49f873a3ccae8c40f052cf0e47c99d6042
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-11-15 12:48:21 +0100

    Add setting in surveys' steps for `allow_editing_answers`

--- decidim-surveys/lib/decidim/surveys/component.rb
+++ decidim-surveys/lib/decidim/surveys/component.rb
@@ -69,6 +69,7 @@ Decidim.register_component(:surveys) do |component|
   component.settings(:step) do |settings|
     settings.attribute :allow_answers, type: :boolean, default: false
     settings.attribute :allow_unregistered, type: :boolean, default: false
+    settings.attribute :allow_editing_answers, type: :boolean, default: true
     settings.attribute :announcement, type: :text, translated: true, editor: true
   end
 
