573f22a452ee77518b96920d622ac9851940c50e
robert <robert.luca@publicissapient.com>
2025-01-16 16:52:12 +0200

    naming fixes

---
+++ decidim-translation-addons/app/models/decidim/translation_addons/report.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Decidim
+  module TranslationAddons
+    class Report < ApplicationRecord
+      belongs_to :resource, foreign_key: "decidim_resource_id", foreign_type: "decidim_resource_type", polymorphic: true
+      belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
+    end
+  end
+end
--- decidim-translation-addons/app/models/decidim/translation_addons/translation_report.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Decidim
-  class TranslationReport < ApplicationRecord
-    belongs_to :resource, foreign_key: "decidim_resource_id", foreign_type: "decidim_resource_type", polymorphic: true
-    belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
-  end
-end
