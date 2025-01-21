392ae53ea10f0f848ab1735810c3e015730f2cf5
robert <robert.luca@publicissapient.com>
2025-01-21 12:16:16 +0200

    change validation on report model

--- decidim-translation_addons/app/models/decidim/translation_addons/report.rb
+++ decidim-translation_addons/app/models/decidim/translation_addons/report.rb
@@ -6,7 +6,8 @@ module Decidim
       REASONS = %w(missing wrong).freeze
       belongs_to :resource, foreign_key: "decidim_resource_id", foreign_type: "decidim_resource_type", polymorphic: true
       belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
-      validates :decidim_resource_id, uniqueness: { scope: [:decidim_resource_type, :field_name, :locale] }
+      validates :decidim_resource_id, uniqueness: { scope: [:decidim_resource_type, :field_name, :locale, :decidim_user_id] }
+      validates :reason, presence: true
     end
   end
 end
