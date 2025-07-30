a61edb5b94c3c7423b52f3f6ef073adbcfdc490c
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-07-30 10:40:37 +0300

    Change the dialog data selector to controller

--- decidim-core/app/packs/src/decidim/index.js
+++ decidim-core/app/packs/src/decidim/index.js
@@ -186,7 +186,16 @@ const initializer = (element = document) => {
 
   element.querySelectorAll('[data-component="dropdown"]').forEach((component) => createDropdown(component))
 
-  element.querySelectorAll("[data-dialog]").forEach((component) => createDialog(component))
+  element.querySelectorAll('[data-controller="dialog"]').forEach((component) => createDialog(component))
+  element.querySelectorAll("[data-dialog]").forEach((component) => {
+    if (component.hasAttribute("data-controller"))
+    {
+      return;
+    }
+    alert(`${window.location.href} Using dialog component`);
+    createDialog(component);
+  })
+
 
   // Initialize available remote modals (ajax-fetched contents)
   element.querySelectorAll("[data-dialog-remote-url]").forEach((elem) => new RemoteModal(elem))
