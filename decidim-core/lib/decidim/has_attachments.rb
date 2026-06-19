ad0cf357cf01a5e397742651c7c1f3f502f5ae6c
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-06-19 04:38:49 +0300

    Fix attachments count

--- decidim-core/lib/decidim/has_attachments.rb
+++ decidim-core/lib/decidim/has_attachments.rb
@@ -13,8 +13,7 @@ module Decidim
                class_name: "Decidim::Attachment",
                dependent: :destroy,
                inverse_of: :attached_to,
-               as: :attached_to,
-               counter_cache: :attachments_count
+               as: :attached_to
 
       # The first attachment that is a photo for this model.
       #
